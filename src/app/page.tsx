7feb06ecb0fbedf800638fb22eceb013251ef9f5
Hussain <sayedhusainabbashaider78600@gmail.com>
2025-04-26 12:10:24 +0530

    Signup, Login 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1 className=' uppercase text-8xl text-wrap wrap-break-word'>Simple and Tasty recipies</h1>
+    </div>
   )
 }
 
