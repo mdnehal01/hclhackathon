91fc186c817e0605b8439199ad4c57ebdb76602e
Hussain <sayedhusainabbashaider78600@gmail.com>
2025-04-26 15:07:04 +0530

    Handover 2

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 const page = () => {
   return (
     <div className='bg-white h-full min-h-screen text-black font-serif px-4 py-2'>
-        
+      <div className='flex flex-col items-center justify-center h-full'>
+        <h1 className='text-4xl font-bold mb-4'>Hello  Admin Page</h1>
+        <div className='flex space-x-4'>
+        </div>
+        <p className='mt-4'>Welcome to the admin page!</p>
+      </div>
     </div>
   )
 }
